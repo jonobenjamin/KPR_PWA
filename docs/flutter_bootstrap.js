f2dfe1c854c30b0e7fd90ab8e46c45c92325b8cb
jonobenjamin <163100744+jonobenjamin@users.noreply.github.com>
2026-02-09 11:23:28 +0200

    Update flutter_bootstrap.js

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"db373eb85a06eb3d706af362a45d42d0972f4b
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2231115877"
+    serviceWorkerVersion: "4207769492"
   }
 });
