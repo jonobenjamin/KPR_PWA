af116344366611a4c62213ca23c6d42197a742ca
jonobenjamin <163100744+jonobenjamin@users.noreply.github.com>
2026-03-05 11:52:44 +0200

    Add files via upload

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "0abcbdf2e290a2f1a915f90962c5bfc7",
+const RESOURCES = {"flutter_bootstrap.js": "eca01bed136eb6ebf096e0ae3a0910f4",
 "version.json": "b359803206879e1d7961102c7506ac90",
 "index.html": "cbb956f6720a915b5fedf77b41245137",
 "/": "cbb956f6720a915b5fedf77b41245137",
-"main.dart.js": "5b393917bb8432688ae3dec03c5ab6cc",
+"main.dart.js": "cdc17a8c1b3bb7f9476485ef99b06587",
 "flutter.js": "24bc71911b75b5f8135c949e27a2984e",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "0658615ef1bdea8a662d5bb1c68d97b6",
